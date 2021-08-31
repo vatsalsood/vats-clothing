f9c71e37374aea09aa147347510c3a1f34637787
vatsalsood23 <vsood@metocean.com>
2021-08-31 16:23:11 -0300

    inital files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import HomePage from "./HomePage/homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage/homepage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <div className="subtitle">SHOP NOW</div>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <div className="subtitle">SHOP NOW</div>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+
+          <div className="subtitle">SHOP NOW</div>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <div className="subtitle">Jackets</div>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+
+          <div className="subtitle">MENS</div>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <div className="subtitle">WOMENS</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
