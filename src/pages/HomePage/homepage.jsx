4216132e2d4d8c550e97952d32a47c5a72ad0efd
vatsalsood23 <vsood@metocean.com>
2021-09-02 16:47:45 -0300

    new components

--- src/pages/HomePage/homepage.jsx
+++ src/pages/HomePage/homepage.jsx
@@ -1,47 +1,10 @@
 import React from "react";
 
+import Directory from "../../components/directory/directory";
+
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <div className="subtitle">SHOP NOW</div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <div className="subtitle">SHOP NOW</div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-
-          <div className="subtitle">SHOP NOW</div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <div className="subtitle">Jackets</div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-
-          <div className="subtitle">MENS</div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <div className="subtitle">WOMENS</div>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
