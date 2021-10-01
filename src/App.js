c72dfc28607730302bc96ce99a22d7915f02c704
vatsalsood23 <vsood@metocean.com>
2021-10-01 09:24:35 -0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/homepage";
 import "./pages/HomePage/homepage.styles.scss";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => {
   return (
     <div className={`${size} menu-item`}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
       ></div>
       <div className="content">
         <h1 className="title">{title}</h1>
-        <div className="subtitle">Jackets</div>
+        <div className="subtitle">{title}</div>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
